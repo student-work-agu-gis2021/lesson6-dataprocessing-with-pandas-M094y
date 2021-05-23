9432973b8d9eb4d983da90b946092ebe47c47f4c
M094y <>
2021-05-23 16:14:28 +0000

    I added the variable to find the average temperature for the whole data file.

--- Exercise_6_Problems_1_2.py
+++ Exercise_6_Problems_1_2.py
@@ -18,6 +18,11 @@ data = None
 
 # YOUR CODE HERE 1
 
+fp = r'data/1091402.txt'
+data = pd.read_csv(fp, delim_whitespace=True, skiprows=[1], na_values=[-9999])
+print(data.head())
+print(data.tail())
+
 # ### Part 2 
 # 
 # In this section, you will calculate simple statistics based on the input data:
@@ -27,7 +32,7 @@ data = None
 
 tavg_nodata_count = None
 #YOUR CODE HERE 2
-
+tavg_nodata_count = data.iloc[:,6].isnull().sum()
 
 #CAUTION!!! DON'T EDIT THIS PART START
 # Print out the solution:
@@ -40,7 +45,7 @@ print('Number of no-data values in column "TAVG":',tavg_nodata_count)
 
 tmin_nodata_count = None
 #YOUR CODE HERE 3
-
+tmin_nodata_count = data.iloc[:,8].isnull().sum()
 #CAUTION!!! DON'T EDIT THIS PART START
 # Print out the solution:
 print('Number of no-data values in column "TMIN":', tmin_nodata_count)
@@ -52,7 +57,7 @@ print('Number of no-data values in column "TMIN":', tmin_nodata_count)
 
 day_count = None 
 #YOUR CODE HERE 4
-
+day_count = len(data)
 #CAUTION!!! DON'T EDIT THIS PART START
 # Print out the solution:
 print("Number of days:", day_count)
@@ -65,6 +70,7 @@ print("Number of days:", day_count)
 first_obs = None
  
 # YOUR CODE HERE 5
+first_obs = data.iloc[0,4]
 
 #CAUTION!!! DON'T EDIT THIS PART START
 # Print out the solution:
@@ -77,7 +83,7 @@ print('Date of the first observation:',first_obs)
 last_obs = None
 
 # YOUR CODE HERE 6
-
+last_obs = data.iloc[day_count-1,4]
 #CAUTION!!! DON'T EDIT THIS PART START
 # Print out the solution:
 print('Date of the last observation:', last_obs)
@@ -90,7 +96,7 @@ print('Date of the last observation:', last_obs)
 avg_temp = None
 
 # YOUR CODE HERE 7
-
+avg_temp = np.mean(data.iloc[:,6])
 #CAUTION!!! DON'T EDIT THIS PART START
 # Print out the solution:
 print('Average temperature (F) for the whole dataset:', round(avg_temp, 2))
@@ -116,6 +122,7 @@ print('Average temperature (F) for the Summer of 69:', round(avg_temp_1969, 2))
 monthly_data = None
 
 # YOUR CODE HERE 9
+monthly_data = 
 
 #CAUTION!!! DON'T EDIT THIS PART START
 # This test print should print the length of variable monthly_data
